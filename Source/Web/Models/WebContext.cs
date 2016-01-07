3802efde22bc9ad76d65ee266faf9b0fa62fdf10
d-aguilera <daniel.aguilera@gmail.com>
2016-01-07 20:58:54 -0300

    Added WebContext

---
+++ Source/Web/Models/WebContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Web.Models
+{
+    public class WebContext : DbContext
+    {
+        public WebContext() : base("WebContext")
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
