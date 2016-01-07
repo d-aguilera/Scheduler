106a8e620c19e050b76b1f50551bc39c70712273
d-aguilera <daniel.aguilera@gmail.com>
2016-01-07 20:59:39 -0300

    Added Identifiable, Auditable base model classes

---
+++ Source/Web/Models/Auditable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Web.Models
+{
+    public abstract class Auditable : Identifiable
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public DateTime Created
+        {
+            get;
+            set;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string CreatedBy
+        {
+            get;
+            set;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public DateTime? LastUpdated
+        {
+            get;
+            set;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string LastUpdatedBy
+        {
+            get;
+            set;
+        }
+    }
+}
