ab0238f37566142124ff97012b70feb8c42d38e0
d-aguilera <daniel.aguilera@gmail.com>
2016-01-08 15:49:59 -0300

    Added AgentServiceClient project

---
+++ Source/AgentServiceClient/AgentServiceClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.AgentService.Client
+{
+    public class AgentServiceClientFactory
+    {
+        public static IAgent CreateChannel()
+        {
+            var factory = new ChannelFactory<IAgent>("AgentService");
+            var channel = factory.CreateChannel();
+            return channel;
+        }
+    }
+}
