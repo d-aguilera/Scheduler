5c5d913977970ada1213aee81787bcc8396dd4b4
d-aguilera <daniel.aguilera@gmail.com>
2016-01-08 04:33:00 -0300

    Added AgentService project

---
+++ Source/AgentService/Agent.cs
@@ -0,0 +1,25 @@
+﻿using Scheduler.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Scheduler.AgentService
+{
+    public class Agent : IAgent
+    {
+        public void Execute(string shellCommand)
+        {
+        }
+
+        public void Create(ScheduleEntry scheduleEntry)
+        {
+        }
+
+        public void Update(ScheduleEntry scheduleEntry)
+        {
+        }
+    }
+}
---
+++ Source/AgentService/IAgent.cs
@@ -0,0 +1,23 @@
+﻿using Scheduler.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Scheduler.AgentService
+{
+    [ServiceContract]
+    public interface IAgent
+    {
+        [OperationContract]
+        void Execute(string shellCommand);
+
+        [OperationContract]
+        void Create(ScheduleEntry scheduleEntry);
+
+        [OperationContract]
+        void Update(ScheduleEntry scheduleEntry);
+    }
+}
