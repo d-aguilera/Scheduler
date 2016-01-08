b95983d4539d17c34e6388a64248429a6f1cd51b
d-aguilera <daniel.aguilera@gmail.com>
2016-01-08 03:05:21 -0300

    Major controllers refactorization

---
+++ Source/Web/Controllers/LogEntriesController.cs
@@ -0,0 +1,17 @@
+﻿using Scheduler.Web.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Scheduler.Web.Controllers
+{
+    public class LogEntriesController : IndexControllerBase<LogEntry>
+    {
+        // GET: LogEntries
+        public override ActionResult Index()
+        {
+            var logEntries = Context.LogEntries.Include(l => l.Client);
+            return View(logEntries.ToList());
+        }
+    }
+}
---
+++ Source/Web/Controllers/ScheduleEntriesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Scheduler.Web.Models;
+
+namespace Scheduler.Web.Controllers
+{
+    public class ScheduleEntriesController : ControllerBase<ScheduleEntry>
+    {
+        // GET: ScheduleEntries
+        public override ActionResult Index()
+        {
+            var schedules = Context.Schedules.Include(s => s.Client);
+            return View(schedules.ToList());
+        }
+
+        // POST: ScheduleEntries/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public override ActionResult Create([Bind(Include = "Id,ClientId,CronExpression,ShellCommand,Enabled,Created,CreatedBy,LastUpdated,LastUpdatedBy")] ScheduleEntry scheduleEntry)
+        {
+            return base.Create(scheduleEntry);
+        }
+
+        // POST: ScheduleEntries/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public override ActionResult Edit([Bind(Include = "Id,ClientId,CronExpression,ShellCommand,Enabled,Created,CreatedBy,LastUpdated,LastUpdatedBy")] ScheduleEntry scheduleEntry)
+        {
+            return base.Edit(scheduleEntry);
+        }
+
+        protected override void AddSelectListsToViewBag(Identifiable entity = default(Identifiable))
+        {
+            ViewBag.ClientId = entity == default(Identifiable)
+                ? new SelectList(Context.Clients, "Id", "NetworkName")
+                : new SelectList(Context.Clients, "Id", "NetworkName", ((ScheduleEntry)entity).ClientId)
+                ;
+        }
+    }
+}
