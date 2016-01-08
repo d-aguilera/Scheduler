c3f38217d243a91cf8f40c0a2ac9513699311507
d-aguilera <daniel.aguilera@gmail.com>
2016-01-07 21:00:10 -0300

    Added Client model, controller, views

---
+++ Source/Web/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Web.Models
+{
+    public class Client : Auditable
+    {
+        [Required]
+        public string NetworkName
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
