ac35c24dd356b83c237c78cc6abb7c37eac9f168
d-aguilera <daniel.aguilera@gmail.com>
2016-01-07 21:00:25 -0300

    Added Schedule model, controller, views

---
+++ Source/Web/Models/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Web.Models
+{
+    public class Schedule : Auditable
+    {
+        public int ClientId
+        {
+            get;
+            set;
+        }
+
+        public string CronExpression
+        {
+            get;
+            set;
+        }
+
+        public string ShellCommand
+        {
+            get;
+            set;
+        }
+
+        public bool Enabled
+        {
+            get;
+            set;
+        }
+    }
+}
