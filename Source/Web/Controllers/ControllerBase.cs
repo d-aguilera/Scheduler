0f6316335da981113cabc6370e6c372df2f70e86
d-aguilera <daniel.aguilera@gmail.com>
2016-01-08 02:51:53 -0300

    Added abstract generic controllers

---
+++ Source/Web/Controllers/ControllerBase.cs
@@ -0,0 +1,125 @@
+﻿using Scheduler.Web.Models;
+using System;
+using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Scheduler.Web.Controllers
+{
+    public abstract class ControllerBase<TEntity> : IndexControllerBase<TEntity> where TEntity : Identifiable
+    {
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        // GET: ScheduleEntries/Create
+        public virtual ActionResult Create()
+        {
+            AddSelectListsToViewBag();
+            return View();
+        }
+
+        // POST: Entities/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public virtual ActionResult Create(TEntity entity)
+        {
+            if (ModelState.IsValid)
+            {
+                SetDefaultValuesForCreate(entity);
+                var set = Context.Set<TEntity>();
+                set.Add(entity);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            AddSelectListsToViewBag(entity);
+            return View(entity);
+        }
+
+        // GET: Entities/Edit/5
+        public virtual ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var set = Context.Set<TEntity>();
+            var entity = set.Find(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            AddSelectListsToViewBag(entity);
+            return View(entity);
+        }
+
+        // POST: Entities/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public virtual ActionResult Edit(TEntity entity)
+        {
+            if (ModelState.IsValid)
+            {
+                SetDefaultValuesForEdit(entity);
+                Context.Entry(entity).State = EntityState.Modified;
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            AddSelectListsToViewBag(entity);
+            return View(entity);
+        }
+
+        // GET: Entities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var set = Context.Set<TEntity>();
+            var entity = set.Find(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+        // POST: Entities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var set = Context.Set<TEntity>();
+            TEntity client = set.Find(id);
+            set.Remove(client);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected virtual void SetDefaultValuesForCreate(Identifiable entity)
+        {
+            var auditable = entity as Auditable;
+            if (null != auditable)
+            {
+                auditable.Created = DateTime.UtcNow;
+                auditable.CreatedBy = User.Identity.Name;
+                auditable.LastUpdated = null;
+                auditable.LastUpdatedBy = null;
+            }
+        }
+
+        protected virtual void SetDefaultValuesForEdit(Identifiable entity)
+        {
+            var auditable = entity as Auditable;
+            if (null != auditable)
+            {
+                auditable.LastUpdated = DateTime.UtcNow;
+                auditable.LastUpdatedBy = User.Identity.Name;
+            }
+        }
+
+        protected virtual void AddSelectListsToViewBag(Identifiable entity = default(Identifiable))
+        {
+        }
+    }
+}
---
+++ Source/Web/Controllers/IndexControllerBase.cs
@@ -0,0 +1,52 @@
+﻿using Scheduler.Web.Models;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Scheduler.Web.Controllers
+{
+    public abstract class IndexControllerBase<TEntity> : Controller where TEntity : Identifiable
+    {
+        private WebContext _dbContext = new WebContext();
+
+        protected WebContext Context
+        {
+            get
+            {
+                return _dbContext;
+            }
+        }
+
+        // GET: Entities
+        public virtual ActionResult Index()
+        {
+            var set = Context.Set<TEntity>();
+            return View(set.ToList());
+        }
+
+        // GET: Entities/Details/5
+        public virtual ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var set = Context.Set<TEntity>();
+            TEntity entity = set.Find(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
