9d4123daf7d5a99168d85de5a23f5d96f35fe337
d-aguilera <daniel.aguilera@gmail.com>
2016-01-08 02:58:13 -0300

    Removed unused Web namespace and minor refactorizations

--- Source/Web/Properties/AssemblyInfo.cs
+++ Source/Web/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Web")]
+[assembly: AssemblyTitle("Scheduler.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Web")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Scheduler")]
+[assembly: AssemblyCopyright("Copyright © CrossOver 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
