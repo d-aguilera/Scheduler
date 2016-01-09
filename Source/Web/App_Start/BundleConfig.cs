cdd35bc439ce9b62d41ca1244ce5a395f0c49a79
d-aguilera <daniel.aguilera@gmail.com>
2016-01-09 02:54:24 -0300

    Added Toastr library, minor UI enhancements

--- Source/Web/App_Start/BundleConfig.cs
+++ Source/Web/App_Start/BundleConfig.cs
@@ -23,9 +23,15 @@ namespace Scheduler.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
+                      "~/Scripts/toastr.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/toastr").Include(
+                      "~/Content/toastr.css"));
         }
     }
 }
