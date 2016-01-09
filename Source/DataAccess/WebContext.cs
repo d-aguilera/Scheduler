be71d561a888ec6f34757f72e60ddfaef532922a
d-aguilera <daniel.aguilera@gmail.com>
2016-01-09 02:55:22 -0300

    Added DataAccess project

---
+++ Source/DataAccess/WebContext.cs
@@ -0,0 +1,22 @@
+﻿using Scheduler.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.DataAccess
+{
+    public class WebContext : DbContext
+    {
+        public WebContext() : base("WebContext")
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<ScheduleEntry> ScheduleEntries { get; set; }
+
+        public DbSet<LogEntry> LogEntries { get; set; }
+    }
+}
