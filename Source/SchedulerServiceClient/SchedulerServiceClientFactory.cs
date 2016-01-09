82133c9f24aa984575fe36efc5c5c820106dcf72
d-aguilera <daniel.aguilera@gmail.com>
2016-01-09 03:05:53 -0300

    Added SchedulerServiceClient project

---
+++ Source/SchedulerServiceClient/SchedulerServiceClientFactory.cs
@@ -0,0 +1,25 @@
+﻿using Scheduler.Common;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.SchedulerService.Client
+{
+    public class SchedulerServiceClientFactory
+    {
+        public static IScheduler CreateChannel()
+        {
+            var factory = new ChannelFactory<IScheduler>("SchedulerService");
+            factory.Credentials.UserName.UserName = Helpers.FromBase64(ConfigurationManager.AppSettings["UserName"]);
+            factory.Credentials.UserName.Password = Helpers.FromBase64(ConfigurationManager.AppSettings["Password"]);
+
+            var channel = factory.CreateChannel();
+            return channel;
+        }
+    }
+}
