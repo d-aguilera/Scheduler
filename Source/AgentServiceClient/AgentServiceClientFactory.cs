60d924fb07a201355fcc6848b632c9f842960a95
d-aguilera <daniel.aguilera@gmail.com>
2016-01-09 03:08:51 -0300

    Fixes to Agent service client factory

--- Source/AgentServiceClient/AgentServiceClientFactory.cs
+++ Source/AgentServiceClient/AgentServiceClientFactory.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Scheduler.Common;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -9,10 +12,25 @@ namespace Scheduler.AgentService.Client
 {
     public class AgentServiceClientFactory
     {
-        public static IAgent CreateChannel()
+        public static IAgent CreateChannel(string networkName, int? port, string virtualDirectory)
         {
             var factory = new ChannelFactory<IAgent>("AgentService");
-            var channel = factory.CreateChannel();
+            factory.Credentials.UserName.UserName = Helpers.FromBase64(ConfigurationManager.AppSettings["UserName"]);
+            factory.Credentials.UserName.Password = Helpers.FromBase64(ConfigurationManager.AppSettings["Password"]);
+
+            var uriString = string.Format(
+                CultureInfo.InvariantCulture,
+                "http://{0}{1}{2}/Scheduler/Agent.svc",
+                networkName,
+                null == port ? null : (":" + Convert.ToString(port, CultureInfo.InvariantCulture)),
+                string.IsNullOrWhiteSpace(virtualDirectory) ? null : ("/" + virtualDirectory.Trim())
+                );
+            var uri = new Uri(uriString, UriKind.Absolute);
+            var address = new EndpointAddress(uri);
+
+            var channel = factory.CreateChannel(address);
+            
+
             return channel;
         }
     }
