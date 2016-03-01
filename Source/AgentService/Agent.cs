f1ab42a9fd0541641cd244c14fd30bac1d36e47d
d-aguilera <daniel.aguilera@gmail.com>
2016-03-01 10:10:53 -0300

    Refactored common service configuration logic to a base class

--- Source/AgentService/Agent.cs
+++ Source/AgentService/Agent.cs
@@ -2,34 +2,13 @@
 using Scheduler.ServiceContracts;
 using System;
 using System.ComponentModel;
-using System.Configuration;
 using System.Diagnostics;
-using System.Security.Cryptography.X509Certificates;
 using System.Security.Permissions;
-using System.ServiceModel;
-using System.ServiceModel.Description;
 
 namespace Scheduler.AgentService
 {
-    public class Agent : IAgent
+    public class Agent : SchedulerServiceBase<IAgent>, IAgent
     {
-        public static void Configure(ServiceConfiguration config)
-        {
-            var baseUrl = ConfigurationManager.AppSettings["Scheduler.Service.BaseUrl"];
-            var contract = ContractDescription.GetContract(typeof(IAgent));
-            var binding = new SchedulerBinding();
-            var agentUri = new Uri(baseUrl + "/cert/Agent.svc");
-            var mexUri = new Uri(baseUrl + "/mex");
-            var address = new EndpointAddress(agentUri, EndpointIdentity.CreateDnsIdentity("localhost"));
-            var endpoint = new ServiceEndpoint(contract, binding, address);
-            var sn = ConfigurationManager.AppSettings["Scheduler.Service.CertificateSerialNumber"];
-            config.AddServiceEndpoint(endpoint);
-            config.Credentials.ServiceCertificate.SetCertificate(StoreLocation.LocalMachine, StoreName.My, X509FindType.FindBySerialNumber, sn);
-            config.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.UseAspNetRoles;
-            config.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true, HttpGetUrl = mexUri });
-            config.Description.Behaviors.Add(new ServiceDebugBehavior { IncludeExceptionDetailInFaults = true });
-        }
-
         [PrincipalPermission(SecurityAction.Demand, Name = PrincipalNames.SchedulerService)]
         public void Execute(int logEntryId, string shellCommand, string workingDirectory)
         {
---
+++ Source/ServiceContracts/SchedulerServiceBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Scheduler.ServiceContracts
+{
+    public class SchedulerServiceBase<TChannel>
+    {
+        public static void Configure(ServiceConfiguration config)
+        {
+            var baseUrl = ConfigurationManager.AppSettings["Scheduler.Service.BaseUrl"];
+            var contract = ContractDescription.GetContract(typeof(TChannel));
+            var binding = new SchedulerBinding();
+            var agentUri = new Uri(baseUrl + "/cert/" + typeof(TChannel).Name.Substring(1) + ".svc");
+            var mexUri = new Uri(baseUrl + "/mex");
+            var address = new EndpointAddress(agentUri, EndpointIdentity.CreateDnsIdentity("localhost"));
+            var endpoint = new ServiceEndpoint(contract, binding, address);
+            var sn = ConfigurationManager.AppSettings["Scheduler.Service.CertificateSerialNumber"];
+            config.AddServiceEndpoint(endpoint);
+            config.Credentials.ServiceCertificate.SetCertificate(StoreLocation.LocalMachine, StoreName.My, X509FindType.FindBySerialNumber, sn);
+            config.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.UseAspNetRoles;
+            config.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true, HttpGetUrl = mexUri });
+            config.Description.Behaviors.Add(new ServiceDebugBehavior { IncludeExceptionDetailInFaults = true });
+        }
+    }
+}
